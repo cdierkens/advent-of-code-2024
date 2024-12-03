aa5fb0171196c9cd9fde58a15b7043c3fa60fa98
Christopher Dierkens <chris@dierkens.dev>
2024-12-03 11:10:54 -0500

    feat: day 3

--- day-3/a.ts
+++ day-3/a.ts
@@ -1,9 +1,22 @@
 import { runSolution } from '../utils.ts';
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function mul(a, b) {
+  return a * b;
+}
 /** provide your solution as the return of this function */
 export async function day3a(data: string[]) {
-  console.log(data);
-  return 0;
+  let code = '';
+
+  for (const datum of data) {
+    const instructions = datum.matchAll(/mul\(\d+,\d+\)/g);
+
+    code += code.length
+      ? `+${instructions.toArray().join('+')}`
+      : instructions.toArray().join('+');
+  }
+
+  return eval(code);
 }
 
 await runSolution(day3a);
--- day-3/b.ts
+++ day-3/b.ts
@@ -1,9 +1,19 @@
 import { runSolution } from '../utils.ts';
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function mul(a, b) {
+  return a * b;
+}
 /** provide your solution as the return of this function */
-export async function day3b(data: string[]) {
-  console.log(data);
-  return 0;
+export async function day3a(data: string[]) {
+  // https://regex101.com/r/qimU8O/1
+  const input = data.join('').replaceAll(/don't\(\).+?(do\(\)|$)/g, '');
+
+  const instructions = input.matchAll(/mul\(\d+,\d+\)/g);
+
+  const code = instructions.toArray().join('+');
+
+  return eval(code);
 }
 
-await runSolution(day3b);
+await runSolution(day3a);
