d7342420b9f951c32d6e75feb23a9472a49fbaa6
Christopher Dierkens <chris@dierkens.dev>
2024-12-04 15:06:18 -0500

    feat: day 4 part 2

--- day-4/b.ts
+++ day-4/b.ts
@@ -1,9 +1,53 @@
 import { runSolution } from '../utils.ts';
 
 /** provide your solution as the return of this function */
-export async function day4b(data: string[]) {
-  console.log(data);
-  return 0;
+export async function day4b(rows: string[]) {
+  let total = 0;
+  for (let i = 0; i < rows.length; i++) {
+    const column = rows[i];
+
+    for (let j = 0; j < column.length; j++) {
+      const char = rows[i][j];
+      if (char !== 'A') {
+        continue;
+      }
+
+      let count = 0;
+
+      if (
+        rows[i - 1] &&
+        rows[i + 1] &&
+        rows[i - 1][j - 1] &&
+        rows[i - 1][j + 1] &&
+        rows[i + 1][j + 1] &&
+        rows[i + 1][j - 1]
+      ) {
+        // check NW for M and SE for S
+        // check NW for S and SE for M
+        if (
+          (rows[i - 1][j - 1] === 'M' && rows[i + 1][j + 1] === 'S') ||
+          (rows[i - 1][j - 1] === 'S' && rows[i + 1][j + 1] === 'M')
+        ) {
+          count++;
+        }
+
+        // check NE for M and SW for S
+        // check NE for S and SW for M
+        if (
+          (rows[i - 1][j + 1] === 'M' && rows[i + 1][j - 1] === 'S') ||
+          (rows[i - 1][j + 1] === 'S' && rows[i + 1][j - 1] === 'M')
+        ) {
+          count++;
+        }
+      }
+
+      // if 2 found, add to global counter
+      if (count == 2) {
+        total++;
+      }
+    }
+  }
+  return total;
 }
 
 await runSolution(day4b);
