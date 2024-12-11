c70f8b3627d853d1a0d91feaecab60ad6b75db87
Christopher Dierkens <chris@dierkens.dev>
2024-12-11 15:55:17 -0500

    feat: day 7 part 2

--- day-7/b.ts
+++ day-7/b.ts
@@ -1,9 +1,61 @@
 import { runSolution } from '../utils.ts';
 
-/** provide your solution as the return of this function */
-export async function day7b(data: string[]) {
-  console.log(data);
-  return 0;
+function generateCombinations(options: string[], length): string[] {
+  if (length === 0) return [''];
+
+  const smallerCombinations = generateCombinations(options, length - 1);
+  const result = [];
+
+  for (const combination of smallerCombinations) {
+    for (const option of options) {
+      result.push(combination + option);
+    }
+  }
+
+  return result;
 }
 
-await runSolution(day7b);
+/** provide your solution as the return of this function */
+export async function day7a(data: string[]) {
+  let calibrationTotal = 0;
+
+  row: for (const datum of data) {
+    const [left, right] = datum.split(':');
+
+    const target = parseInt(left, 10);
+    const values = right
+      .trim()
+      .split(/\s+/g)
+      .map((value) => parseInt(value, 10));
+
+    const combinations = generateCombinations(
+      ['+', '*', '|'],
+      values.length - 1
+    );
+
+    for (let i = 0; i < combinations.length; i++) {
+      const combination = combinations[i];
+      let total = values[0];
+
+      for (let j = 1; j < values.length; j++) {
+        const operation = combination[j - 1];
+        if (operation === '+') {
+          total = total + values[j];
+        } else if (operation === '*') {
+          total = total * values[j];
+        } else if (operation === '|') {
+          total = parseInt(`${total}${values[j]}`);
+        }
+      }
+
+      if (total === target) {
+        calibrationTotal += total;
+        continue row;
+      }
+    }
+  }
+
+  return calibrationTotal;
+}
+
+await runSolution(day7a);
