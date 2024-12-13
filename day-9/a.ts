c24c3c8be2d3b6a3f8f334dd478145e3c48aeccf
Christopher Dierkens <chris@dierkens.dev>
2024-12-12 22:39:48 -0500

    feat: day 1 part 1

--- day-9/a.ts
+++ day-9/a.ts
@@ -2,8 +2,48 @@ import { runSolution } from '../utils.ts';
 
 /** provide your solution as the return of this function */
 export async function day9a(data: string[]) {
-  console.log(data);
-  return 0;
+  const [diskMap] = data;
+
+  const disk: string[] = [];
+
+  let id = 0;
+  for (let i = 0; i < diskMap.length; i++) {
+    disk.push(
+      ...Array.from({ length: parseInt(diskMap[i], 10) }, () =>
+        i % 2 === 0 ? id.toString() : '.'
+      )
+    );
+    if (i % 2 === 0) {
+      id++;
+    }
+  }
+
+  let freeIndex = disk.indexOf('.');
+
+  for (let i = disk.length - 1; i > freeIndex; i--) {
+    const char = disk[i];
+
+    if (char === '.') {
+      continue;
+    }
+
+    [disk[i], disk[freeIndex]] = [disk[freeIndex], disk[i]];
+
+    freeIndex = disk.indexOf('.', freeIndex + 1);
+  }
+
+  let checksum = 0;
+  for (let i = 0; i < disk.length; i++) {
+    const char = disk[i];
+
+    if (char === '.') {
+      break;
+    }
+
+    checksum += parseInt(char, 10) * i;
+  }
+
+  return checksum;
 }
 
 await runSolution(day9a);
